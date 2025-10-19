81f98da9f1e14d6bd7f5ff332de056dee41b6dcf
Santiago Martinez <santiagomarse@hotmail.com>
2025-10-19 09:42:57 -0500

    set up firebase with project

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,11 @@ allprojects {
     }
 }
 
+plugins {
+    // Firebase Google Services plugin
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
